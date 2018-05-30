b1108329f8b44349952c828b7c9332736adec3dc
EdwinArciaga <38231411+EdwinArciaga@users.noreply.github.com>
2018-05-29 22:43:11 -0700

    Update Memory.vhd

--- Lab 5/Memory.vhd
+++ Lab 5/Memory.vhd
@@ -37,10 +37,21 @@ begin
     if falling_edge(Clock) then
 	-- Add code to write data to RAM
 	-- Use to_integer(unsigned(Address)) to index the i_ram array
+	    if (WE = '1') THEN -- ONLY write data to i_ram on falling edge and write enable = 1.	
+		if (to_integer(unsigned(Address)) <= 127) then
+			i_ram (to_integer(unsigned(Address))) <= DataIn;
+		end if;
+	end if;
 	
     end if;
 
 	-- Rest of the RAM implementation
+	if (OE='0' AND (to_integer(unsigned(Address)) <=127)) THEN
+	DataOut <=  i_ram (to_integer(unsigned(Address)));
+    else
+	Dataout <= highz;
+    end if;
+	
 
   end process RamProc;
 
@@ -71,8 +82,90 @@ architecture remember of Registers is
 		 dataout: out std_logic_vector(31 downto 0));
 	end component;
 	
+	--INPUT AND OUTPUT FOR ZERO REGISTER--
+	signal zeri: std_logic_vector(31 downto 0);
+        signal zeroo: std_logic_vector(31 downto 0);
+	signal zer: std_logic_vector(31 downto 0):=X"00000000";
+
+	--------INPUT FOR A0 - A7--------
+	signal a0o: std_logic_vector(31 downto 0);
+	signal a1o: std_logic_vector(31 downto 0);
+	signal a2o: std_logic_vector(31 downto 0);
+	signal a3o: std_logic_vector(31 downto 0);
+	signal a4o: std_logic_vector(31 downto 0);
+	signal a5o: std_logic_vector(31 downto 0);
+	signal a6o: std_logic_vector(31 downto 0);
+	signal a7o: std_logic_vector(31 downto 0);
+	
+	--------OUTPUT FOR A0 - A7 -------
+	signal a0i: std_logic_vector(31 downto 0);
+	signal a1i: std_logic_vector(31 downto 0);
+	signal a2i: std_logic_vector(31 downto 0);
+	signal a3i: std_logic_vector(31 downto 0);
+	signal a4i: std_logic_vector(31 downto 0);
+	signal a5i: std_logic_vector(31 downto 0);
+	signal a6i: std_logic_vector(31 downto 0);
+	signal a7i: std_logic_vector(31 downto 0);
+	signal highz: std_logic_vector(31 DOWNTO 0):= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+	
 begin
     -- Add your code here for the Register Bank implementation
+	---DEMUX FOR CHOOSING REGISTER TO WRITE---
+	zeri <= zer;      --when WriteCmd&writereg="100000";
+	a0i <= writeData when WriteCmd&writereg="100001";
+	a1i <= writeData when WriteCmd&writereg="100010";
+	a2i <= writeData when WriteCmd&writereg="100011";
+	a3i <= writeData when WriteCmd&writereg="100100";
+	a4i <= writeData when WriteCmd&writereg="100101";
+	a5i <= writeData when WriteCmd&writereg="100110";
+	a6i <= writeData when WriteCmd&writereg="100111";
+	a7i <= writeData when WriteCmd&writereg="101000";
+	
+	
+	
+	---MUX FOR CHOOSING OUTPUT REGISTER 1---
+	with ReadReg1 select
+		            ReadData1 <= zeroo when "00000",
+				           a0o when "00001",
+					   a1o when "00010",
+					   a2o when "00011",
+					   a3o when "00100",
+					   a4o when "00101",
+					   a5o when "00110",
+					   a6o when "00111",
+					   a7o when "01000",
+	                                 highz when others;
+	
+	
+	
+	
+	---MuX FOR CHOOSING OUTPUT REGISTER 2---
+		with ReadReg2 select
+		            ReadData2 <= zeroo when "00000",
+				           a0o when "00001",
+					   a1o when "00010",
+					   a2o when "00011",
+					   a3o when "00100",
+					   a4o when "00101",
+					   a5o when "00110",
+					   a6o when "00111",
+					   a7o when "01000",
+	                                 highz when others;
+	
+	
+	
+--out: active low
+--in: active high
+------------------------        datin oe32 oe16 oe8      we32    we16  we8   datout
+        x0: register32  PORT MAP(zeri, '0', '1', '1', WriteCmd, '0',  '0', zeroo);
+	a0: register32  PORT MAP(a0i, '0', '1', '1', WriteCmd, '0',  '0', a0o);
+	a1: register32  PORT MAP(a1i, '0', '1', '1', WriteCmd, '0',  '0', a1o);
+	a2: register32  PORT MAP(a2i, '0', '1', '1', WriteCmd, '0',  '0', a2o);
+	a3: register32  PORT MAP(a3i, '0', '1', '1', WriteCmd, '0',  '0', a3o);
+	a4: register32  PORT MAP(a4i, '0', '1', '1', WriteCmd, '0',  '0', a4o);
+	a5: register32  PORT MAP(a5i, '0', '1', '1', WriteCmd, '0',  '0', a5o);
+	a6: register32  PORT MAP(a6i, '0', '1', '1', WriteCmd, '0',  '0', a6o);
+	a7: register32  PORT MAP(a7i, '0', '1', '1', WriteCmd, '0',  '0', a7o);
 
 end remember;
 
