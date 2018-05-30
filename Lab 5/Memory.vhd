77d3d0e0de99c84aadcb6e5c29611b7c9606cc55
EdwinArciaga <38231411+EdwinArciaga@users.noreply.github.com>
2018-05-29 22:44:22 -0700

    Update Memory.vhd

--- Lab 5/Memory.vhd
+++ Lab 5/Memory.vhd
@@ -168,5 +168,124 @@ begin
 	a7: register32  PORT MAP(a7i, '0', '1', '1', WriteCmd, '0',  '0', a7o);
 
 end remember;
+		
+------------ single bit flip flop register--------------------------------
+
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity bitstorage is
+	port(bitin: in std_logic;
+		 enout: in std_logic;
+		 writein: in std_logic;
+		 bitout: out std_logic);
+end entity bitstorage;
+
+architecture memlike of bitstorage is
+	signal q: std_logic := '0';
+begin
+	process(writein) is
+	begin
+		if (rising_edge(writein)) then
+			q <= bitin;
+		end if;
+	end process;
+	
+	-- Note that data is output only when enout = 0	
+	bitout <= q when enout = '0' else 'Z';
+end architecture memlike;
+
+
+
+
+----- register 8 --------------------------------------------------
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity register8 is
+	port(datain: in std_logic_vector(7 downto 0);
+	     enout:  in std_logic;
+	     writein: in std_logic;
+	     dataout: out std_logic_vector(7 downto 0));
+end entity register8;
+
+
+architecture memmy of register8 is
+
+	component bitstorage
+		port(bitin: in std_logic;
+		 	 enout: in std_logic;
+		 	 writein: in std_logic;
+		 	 bitout: out std_logic);
+
+	end component;
+begin
+
+	m0: bitstorage port map(datain(0),enout,writein, dataout(0));
+	m1: bitstorage port map(datain(1),enout,writein, dataout(1));
+	m2: bitstorage port map(datain(2),enout,writein, dataout(2));
+	m3: bitstorage port map(datain(3),enout,writein, dataout(3));
+	m4: bitstorage port map(datain(4),enout,writein, dataout(4));
+	m5: bitstorage port map(datain(5),enout,writein, dataout(5));
+	m6: bitstorage port map(datain(6),enout,writein, dataout(6));
+	m7: bitstorage port map(datain(7),enout,writein, dataout(7));
+end architecture memmy;
+
+-------- 32 bit register ----------------------------------------------
+
+-- EnableOut: 
+-- 0: Active     - Enable output data. Data out
+-- 1: Not Active - Disable output data. No data out
+
+-- WriteIn
+-- 0: Not Active - Don't write in
+-- 1: Active     - Allow write in
+--
+
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity register32 is
+	port(datain: in std_logic_vector(31 downto 0);
+		 enout32,enout16,enout8: in std_logic;
+		 writein32, writein16, writein8: in std_logic;
+		 dataout: out std_logic_vector(31 downto 0));
+end entity register32;
+
+architecture biggermem of register32 is
+
+        signal w32, w16, w8 : STD_LOGIC :='0';
+        signal out32, out16, out8 : STD_LOGIC :='1';
+
+	component register8 is
+	port(datain: in std_logic_vector(7 downto 0);
+	     enout:  in std_logic;
+	     writein: in std_logic;
+	     dataout: out std_logic_vector(7 downto 0));
+	end component;
+begin
+  
+        w8 <= writein8  OR writein16 OR writein32; -- 8 or 16 or 32. In any case 8bits will get written
+        w16 <= writein16 OR writein32;              -- 16 or 32. In any case 16 bits
+        w32 <= writein32;                           -- 32 bits
+
+	out8 <= enout8 AND enout16 AND enout32;     
+        out16 <= enout16 AND enout32;
+        out32 <= enout32;
+
+
+
+	m0: register8 port map(datain(7 downto 0),   out8,   w8,  dataout(7 downto 0));
+	m1: register8 port map(datain(15 downto 8),  out16,  w16, dataout(15 downto 8));
+	m2: register8 port map(datain(23 downto 16), out32,  w32, dataout(23 downto 16));
+	m3: register8 port map(datain(31 downto 24), out32,  w32, dataout(31 downto 24));
+
+end architecture biggermem;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
